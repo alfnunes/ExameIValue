9cc51338acfd50f03b0dbc2de590fd1ca7ebd687
alfnunes <andrenunes2005@gmail.com>
2014-06-04 16:48:59 -0300

    Adicionando Respostas do Teste IValue
    Adicionando O projeto .NET e o documento word com as respostas do teste

---
+++ TestIValue/TestIValueAndreNunes/TestIValueAndreNunes/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestIValueAndreNunes
+{
+    public class Program
+    {
+        #region [ Propriedades ]
+
+        private static List<string> LstPalavrasFoo = new List<string> { "t", "s", "w", "l", "h" };
+        internal const string PalavrasTextoA = "ppj qmlltdnb fqd xljs zjc qnv hcqcgfv fvnwnmks pbjhdk fkhwjq mrbr dfc kdv wsgd xzhmfvl lhtfz smx dpxnm nsqdwx dvpg gwhtbzfn vqkx cfxbb hsh tgmxrgvp wxqqfw rztpvf sqtfj dmdvg bxqw zmc sbdxq vjk fzjn fqnp lcmt dqpt xznnhhsl cxsfnpsd fggjscv svhn ktlzdr xkxrmss vngbl ktqftht nnlvzt whjp mbl jhr fgnnrvfb nkjdbjb lmcdk ktx kmgfjgfp skn lqgtqkt tcznq fjmmrrhs gvdpsd dppfct jhwhx dnvznpx dzl jlcpktnw fnf mfghx nzncvqd bbl wgsg zjcr hckp ntz bqs skmcq wssf bkksf vvcpvwb lqv nwlqpc lbwjmsz hfpt gzg dflps ddhtkfwf rrtzskzt fcmg hdgn cklw pchqsc sbbncjt hzwhr jrjfxs kvh hbsbv qkvvl bxr ljzfdbp sltszt bhb srjzj nmdllhqw glgnck fblgxj cnckc khnlnb zbfcc wgdn jjncrl msx kmplvcw cxbgb nzl nhll clbt ghj jkhpml pgmgxrtx hdz lzpw mkgrgl slq rzljl jbdsjpvb fdgmk sbpz hpmb zdnpmdk phnnpjr hzlxxx swxqtf tdggdhf gqhsv fbnvls qlpxpb dxmbcw zjpk whxrjgxt wqqrv zpc bmlbmtb spc hpr rdd tvpxgxtl vrr bghsd lpr tjpngd flwpdv jgkzrl svf mcfzsjb fpz kcvxdwwz ftglgql rqshckm wlz dxnm crgwx vxtztj jzdtsb lnkwnl xwzfkv vkqrldlg ltgck grc tklm pzwxhfwh dchqj znwhrp pnsplpc xvtf hxpvnsg gqx dsqs fprfb qgmv zjzhpws bwjcn rmtsmfn kkqww lhrxdrx grzk qsgx fdjsgks ntxq bxqbgwz vxnn bxbqqf mdzpljbm rms rtq ftmpl mlqggth wtjhtlt kvwlzvd mvftlc ttcbjwdp wkdmwj gdgnrw jnpppd mbhncl vfdqc kpgg fgx gwdmwz fnrk rjwgzt cwzcdzsm ztfwpf ldgrkqmd dng jwvnxpr tdwtcxqg bwxhbxp csktz pzssgjlp hlhtvvks gdqbwg sxhww kmqgxf cxznm svbwkzlm xzdg rcnw gkkngj mhtcf pkkrw bvpk xpf dgzgzjd wmk mkdhsm qflrdln khgrvqn rclk llzhqdv vltnm knqfz kbpsf kmxxtl dcc sgrjnjf hbdhffd qdftrnm fbrkc cqgtmm lbz ktggkv hrhgn rqfrzh ptwwv nwcxcj gbtt pmzv vbf rlq tdgc lthqqtt krnfhdms mtdbfjrj ktp nbkvtsr rfscgmzx qxglx frhtw xdwkv shc cmhwdqf wvpb ggcnrnwb wczdjnwq jfsg vjxs snxbkkw qpjmpdpv ggcqmgwm nwwzgvjl tkbcs nwwwssx scz qfcf jbwdgvd xphmv djhgmkvv jcfgbfhn jfvkq dzk bhcz rxh svr pwf hdb xhct vxbjbgmt ljxz gpjzbgrk wvgpdc crhthwhz fhghwzpz ccmx kvlqn mnfttktc gxbsh qfvvfcw tbff zvbr zlb xsdsllgz krnkvl jvhfdltc gpsp fslvtfgc fhwxxtqc hnhc qnfgckz rswrxsj jmdf flg zdzjg wqnplvgq tczgrmp knr pvzcjs klshcdv npwdzqn tvprqcb bmvcg psqcgqrv klzbq xcwgbb mhtxv bqmjw lxnwtp fhjsbwr rzz bgjdbwbx ftdqwbv dgxdxd htfxg pzfnksb swq dwqm npfmqbt frngqrmb vlvfjfrr nqkqvr tqqqrm dfkkzs tvjn pxldtxfn kmjqj kxpbh jhpqnnmw lzq xgpzhhvt lvpmcb hngcht xvfnspr krf mdcwfmd glll plmrsb vzd vsbzqxg hndpxr mqsqdfmp trghlpx nplzsk qgfld fkq lqkx hqcmvwrc gfgjgph tnst rnhkw rqv ctmfkgcr szqmss nbvswchs mrqpw jgxcvkpq bwqk vtjvgvjs fxrj ppjtxrkg jrbzdrdk lmrnb szdcmk wznf jxznsg bgkddsm rmngkhms tqcvlds xczhht tmmsbgsd czvpcrll xfbjbrhv hmvp qwb hmvbr ltw mhmbvbd nmqncvvh vxbk tczgjs zkkjjsc qkr ttcc vhldzw wwgltg mvq rdnkkkq wwldz chbtgsjl gsmrmgmv rszzqpk pxgqjt cxd zcrnp knstqzd nxl lfbks zznfl mtzwdvp bhnw cqx tdxfcbh vnvfrsr tzt lllz xmjvwndh wgwm gvgvsdnp qgrgfb bwqmfcvl kmkg tvfhcfwx gbk fbvzjps ppblb bvfrhltr gbmpm rxwm vzjhh mxtvt fsmhtf zxnftg klzhmq whvrzxb pwtlfqp tkp gthgbnfq fvnjm rhr rghw vrnpbx wbfxn msqd fvqdzm tfxmvvk jhd chtjvspt sklslf ldhwwtl rcjwtmw wjvlc mwch klbjq gcgpwqrl cjpcht rsgpgn hvkmhsqp pzvjhwnr fdzzg sczwmqz vfbgqg ggz vwbb hrtxxf zxb pkwlg mpkws xxtk hnhgfn lrwhr flg qbwssqdv zgth qshd brbtpfk tcj kfqb skwzjt hhrrrds fhxznc jwkmxhb pgj ljmcds bnkwldn qtkghjpw qhkvdrpd mpcsl jmxqld vkxvlnt vmmg nprqbt sjjg mhrlvwt rbrwn ckb hdlv nwtlwm qwkfkgnk jwhc pfvjlprq qbz phhkn qctrthrs vzb rlmcmj bhvfksd kgd cqchsgw dhppv fjds nclcxzss scc pcjp llzpc pwv ngnvgjp dhvfcgk ppxpffr cmzk hmlwk fpx dhfkmsgm stgpgc znc fzfjfbtz wkzrsvls wsw lzhjxknm svlsjq qkdnlscr bhmgrhfp lmfw kntqq qxrdg wpwlhdgv jvfm dxg kvpr ljsh tcvx mcmlm fjbxzs dqqdx xsh sxr xksghjw kgfbkj prbtf hxvpbth qvkzf snzlrr glqcctr tkfl ctgspcg htzgr zrckgxck xjkk rhbcmhtm gljp cgvq gzmnzpbb bmjhllxr qnzhc nmbrblk fzn pxjzvq jftgq sbr klrwrzt slxhzhq srvvvc lgt dfrxv shdwh chqc gftfgvkv nndvrbr brrtmxzv plvxdj xcjh dfmxrpj tsnqvvd bmkmpt chcfr";
+        internal const string PalavrasTextoB = "zsbb ffmqqzn gpcpl slnwbfk fhvpv pcpp fktvgjtq pwmd txg ltxgksj vfddqdp tdhjkh fzklb mlhpnw vdh vbkgpbgg dmpgr pskvkx zfwxbgr wfwt xvhfmpb swbvpt cdqx rmns rsfqww cjhjfcm nhdztjsb bqsfq fbh zskrp pdzg dglfssk xsgtn hmh nbh tmjn pjzd cxvx dgd djvgql tsjlh rpbcgms sbq bbqcv mvl hjh zhttp kmsnvmqp rdjpvf xwrtndnd bkqvxk rbqgmr kxnwdgqg wbbf gnrl gmksgwg jxwcq wkdhglb mpv lqpdh dbxnkb klhfgs ldsfgpnz wmxcd kcbvdrg hvgbxj cmmkwbqx trg ztwmls mbztlgwk pnvdrhf fpcp mlqjtgzh rwvvt lqnc hfw bmnfppmv zvbggrzl drdfzqb rbhfv zbrcnm fxrdwwh bgnlwr tlzdfznm hlvtwzfq rfslqqlf mth pngg btbzlzkm wkdjnr fvz qfhnfqh rzbwztnr gjjvdpm mzbd ljv dxs xbc wdv gfdm pjmdzb rcszs zrwqddm dpth mmvh xhxfbk gmcchd jtcq xnbzgll tqwjqcg cbgkwjgc pgnvvtr bvr mhnhktbr fhvxkgsf lhgh dskpmfbq nhrddc ckvgk rhfr bkjzgs gjrq xmwt dgp hsjg zctzpbc mxqmdgpb dsslbstd lgff rbkrt zpq srrrwpzf nfdtddp dgjhvsl xwnz qqtnjtv gskklxkw rwnvxjqd znhgbzm jzr ldwszrcm zlmwhjw qwsgvmwk jnz sgdr dbdm rgsjn dxbhlb gfrqtm ldbw hvq zgllvkzd dbpxzn vzcrr thtfl wcc sqzsfcwn qtfswfj ghhxzd wzbxsmvz mblcst wmxwngw pnkqh lzfkdrkf tthj sccvpzp jrjgv sprrl kcp nwrmdcg kbvj gdcd xnxpgjxv kxqtk dgfh qrgkrggg pnzzwm dflv rgp vbgmnmll shb dznlzlb zrs fbwqd rlblp fsl lpxbktg vrhbsrbh qqsd znq mtqh rsrpjllh nqstrx brxtcg nrmmq wdmrrvbx zcnkvnkr jbxrm qskjmpcs mpdtk trjbv bjfxm mmvxw jlncbj gqkdjnpt pkjgbf ndpljpq pgvrzdk qkdj kzbdccxh hxttlnc vxf bsls pfffd xpg gbzk lrlg rpqsh nrgsgqbk ptsfjnsn cpvg dclltm ctl vbwnkhg pcxpr xtmwc htwpkn nsjc jhqvvcpj ljwjx psg vxcswsgn lcf wjmxsstb jvksghb wzrzltsl scg gqwjv vgqgw dpmhsxxf spdg zgmz rvzf tztrv lvfj mprbnjtm qcbv fkxmxvj fxjkkxnl tmqvwv vbmw gdrbzg ccsjhs ssbqx ldplxps lbd tpnlbszm lwknpg fdppmrmd kch rktqd ssvnjwk gsjk pjhjx vfkpht lfpnt hwjsg ghzg mzvjw ffbm dxzn nlftbcn tcxggx qqqqqwb gwprs ktn zqx jxqmdr swl qtdrs dtvjwx klhrcfk gfvcmj cvjv krkd xmz gtr ptg cqqk qwfhjk fvhrx qskqfvfm ktwfg rphkqr kwxjxct lbqzbl bzs jppx vkhvw jcx zgtdrv mrvrj sfr hxflch zcbsj zvrkzx jxtgjmn zsmgw xtvzld dtpblt wrwnpr wnsmm ssf zxxt jnm ktmvkwp rqqztql rwbphms cvnm kmqd jqpqjm hzsnlbqw fnxhm vcjmszt dzw pbvs knt wcg kgwx vpzkwwx jklfswl zchwjx cjtfw zjfhv bpgqwfs jfhhqh rwshxfn shfvd pgcpp tmcz nqjvs dhtbvrmq dthxjvhv vqgn gmbwqwk grms ppthrb ppkmld whjkkkt sdd rcclt vxz pcnjw vdpgcgz qncsxj mprq hpj rgckm brzxf njsljl kscxmv hvn ghr jtppn xgnlcmdl vgl hhwx plnzdcrb rgwc dmqxbnr dctm gpntq qmvgc jgrljg sgjctv dltbbdb wzlhs hfzqf dhvpzgs xrrwtmjk qkcjh tgdlvcg wgbshf bncpcf nlwt btf sqvs psflr gxpsf lmwr nnnbjtt snhqqswk mswhpdp czl fsbxhzcf rrfjczf bkfkwkvn dkm xzq xkqkd vmhhj hcdtvgk hfmnp gjnmkcn wmzp mkjv dznrt bbxgqhg bbgvzzrl kqzkthdx wxmkcl ktfnkfd vfw klhbg cgm qhkmd gzpvnnkg qcwt xkhb bvgwr zgxscvr tsgchwmq mnrt wwb fhnvtzj bzhgwtr mvqj hwmchst vjz mfdwtlgz trgk mkkrfnz fdknhrtk djx cnf zrv twzxb bctfrnpl tfnjth qpbfcq hwk pmxjdzk zmr dsxnfctn vmjvxhl mcf zlthph dpf mlpfkpw hrlz vmdnsh jdls zwpqnfsf krxbk fhqc rrwt hdx ddz bqfmdvw znqm xfxqrcm thzctsxg twcr flxw rfqsdm dhgm ngwptn zrfwmkv xgd dnd wfgwwl gfhthbwj mvqbncg ctgx lhfgjvqt vjvhc sccxjgwh lsrcdx mwscxzx hhrdv hhvmd rsqxqr kcp dkpplfz qkmxckbx wrkqjf qcvldrs nwwmfjq bndtc xwl wcsrzn hxg flspqjb gjtqlbh hbdrfj pgkzv xqrmtcwm tfsjqx fzlgvz krrjdt mdhjcdj xpspdz stscgg vnghts khhwvdhr nqcpbrb ktd xmxfhc pfrm hxnvch mjcldq qsfxg lqtnpkk drvqbv bqmg gxp tdqzbsqh mrvjwtlb qgdcmxp dllt kdklt lshnjhtt lrmv rhvnmr lcj nfhngb hpkz xmh dnxsnj dlgngxr rwdjf hjpvxnwt dwv ldplvxpn rlcscmpf zpdglpg hkbl ssq kvblgw xpmrv tndw krwwskvq fjh jnt xkwpmsb jpfgs mfqftxdp zzm jnnctnq bclkkrtv tdlhv ljtwnv jbdwgpv pkw zwbdjqfm tvlft xkcckxd ldngdzqb jmp dtdtpl pbsn fpjq wjmcrdft pjtcnlww gxmnj cqhpgqx fdtgzh qcjp lxq grbfbwq ptrtpmnz xbkthnhk rpmgm mdksgj stwfcj qbcpprh pfxtdfc mvl ckdmlp dtk gbmjt dvppsrsq fvz zgr cmjnhsvf lwcmpnkf qcdkcq tzh plfbsnsp swwfbfbr wnxwjl drcghs pmxhflhd psffj lgcdnq htbj vkd kkkwn xrz kfft";
+
+        #endregion
+        
+        #region [ Metodos ]
+
+        /// <summary>
+        /// Metodo principal - Main
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            #region [ Questao 1 - Preposicao]
+
+            Console.WriteLine(string.Format("Quantidade de Preposicao texto A: {0} \nQuantidade de Preposicao texto B: {1}", ContadorPreposicao(PalavrasTextoA), ContadorPreposicao(PalavrasTextoB)));
+
+            #endregion
+
+            #region [ Questao 2 - Verbos]
+
+            var objA = ContadorVerbos(PalavrasTextoA);
+            var objB = ContadorVerbos(PalavrasTextoB);
+
+            Console.Write(string.Format("\nQuantidade de Verbos texto A: {0} Quantidade de Primeira Pessoa texto A: {1}", objA.QtdeVerbo, objA.QtdePrimeiraPessoa));
+            Console.Write(string.Format("\nQuantidade de Verbos texto B: {0} Quantidade de Primeira Pessoa texto B: {1}", objB.QtdeVerbo, objB.QtdePrimeiraPessoa));
+
+            #endregion
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Verifica a quantidade de preoposicao do texto
+        /// </summary>
+        /// <remarks>
+        /// Implementa a seguinte regra: preposições => Ivaluesh, sao palavras de 3 letras que terminam numa letra tipo foo, mas nao ocorre a letra m.
+        /// </remarks>
+        /// <param name="palavras">Texto para encontrar a preposicao</param>
+        /// <returns>quantidade de preoposicao</returns>
+        private static int ContadorPreposicao(string palavras)
+        {
+            return (from t in palavras.Split(' ') let eFooENaoContemM = !t.Contains("m") && LstPalavrasFoo.Contains(t.Substring((t.Length - 1), 1)) where t.Length == 3 && eFooENaoContemM select t).Count();
+        }
+
+        /// <summary>
+        /// Verifica a quantidade de Verbos do texto e quantidade de Primeira Pessoa tambem
+        /// </summary>
+        /// <remarks>
+        /// Implementa a seguinte regra: verbos => Ivaluesh, 8 ou mais letras que terminam numa letra tipo foo, se comecar com uma letra tipo foo esta em primeira pessoa.
+        /// </remarks>
+        /// <param name="palavras">Texto para encontrar o verbo e a primeira pessoa</param>
+        /// <returns>quantidade de verbo e primeira pessoa no ObjetoVerbo</returns>
+        private static ObjetoVerbo ContadorVerbos(string palavras)
+        {
+            return new ObjetoVerbo
+            {
+                QtdeVerbo = (from t in palavras.Split(' ') let eFoo = LstPalavrasFoo.Contains(t.Substring((t.Length - 1), 1)) where t.Length >= 8 && eFoo select t).Count(),
+                QtdePrimeiraPessoa = (from t in palavras.Split(' ') let eFoo = LstPalavrasFoo.Contains(t.Substring((t.Length - 1), 1)) let ePrimeiraPessoa = LstPalavrasFoo.Contains(t.Substring(0, 1)) where t.Length >= 8 && eFoo select ePrimeiraPessoa).Count(ePrimeiraPessoa => ePrimeiraPessoa)
+            };
+        }
+
+        #endregion
+        
+        #region [ Classe ]
+
+        private struct ObjetoVerbo
+        {
+            public int QtdeVerbo { get; set; }
+            public int QtdePrimeiraPessoa { get; set; }
+        }
+
+        #endregion
+        
+    }
+}
